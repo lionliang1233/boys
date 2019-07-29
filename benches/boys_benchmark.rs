592756a7f85c9853a4bb1d7986d14bbe25b3bf04
Eric Berquist <eric.berquist@gmail.com>
2019-07-28 22:03:00 -0400

    Make sure the benchmarks compile under Travis

--- benches/boys_benchmark.rs
+++ benches/boys_benchmark.rs
@@ -14,24 +14,25 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("boys_micb25_14", |b| {
         b.iter(|| boys::micb25::boys(14, 42.67768466983068))
     });
-    c.bench_function("boys1_jeffhammond_2", |b| {
-        b.iter(|| boys::jeffhammond::boys1(2, 2.0))
-    });
-    c.bench_function("boys1_jeffhammond_14", |b| {
-        b.iter(|| boys::jeffhammond::boys1(14, 42.67768466983068))
-    });
-    c.bench_function("boys2_jeffhammond_2", |b| {
-        b.iter(|| boys::jeffhammond::boys2(2, 2.0))
-    });
-    c.bench_function("boys2_jeffhammond_14", |b| {
-        b.iter(|| boys::jeffhammond::boys2(14, 42.67768466983068))
-    });
-    c.bench_function("boys3_jeffhammond_2", |b| {
-        b.iter(|| boys::jeffhammond::boys3(2, 2.0))
-    });
-    c.bench_function("boys3_jeffhammond_14", |b| {
-        b.iter(|| boys::jeffhammond::boys3(14, 42.67768466983068))
-    });
+    // TODO these are disabled until their correctness can be determined.
+    // c.bench_function("boys1_jeffhammond_2", |b| {
+    //     b.iter(|| boys::jeffhammond::boys1(2, 2.0))
+    // });
+    // c.bench_function("boys1_jeffhammond_14", |b| {
+    //     b.iter(|| boys::jeffhammond::boys1(14, 42.67768466983068))
+    // });
+    // c.bench_function("boys2_jeffhammond_2", |b| {
+    //     b.iter(|| boys::jeffhammond::boys2(2, 2.0))
+    // });
+    // c.bench_function("boys2_jeffhammond_14", |b| {
+    //     b.iter(|| boys::jeffhammond::boys2(14, 42.67768466983068))
+    // });
+    // c.bench_function("boys3_jeffhammond_2", |b| {
+    //     b.iter(|| boys::jeffhammond::boys3(2, 2.0))
+    // });
+    // c.bench_function("boys3_jeffhammond_14", |b| {
+    //     b.iter(|| boys::jeffhammond::boys3(14, 42.67768466983068))
+    // });
 }
 
 criterion_group!(benches, criterion_benchmark);
